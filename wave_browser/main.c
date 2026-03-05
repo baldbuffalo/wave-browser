4c912e5586336cacdc4c4af9979f8b8c27a496ce
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-03-05 18:52:06 +0400

    Update main.c

--- wave_browser/main.c
+++ wave_browser/main.c
@@ -1,18 +1,24 @@
+#include <proc_ui/procui.h>
+#include <coreinit/foreground.h>
 #include <vpad/input.h>
 #include <unistd.h>
 
+static void SaveCallback(void) {
+    OSSavesDone_ReadyToRelease();
+}
+
 int main(void) {
+    ProcUIInit(&SaveCallback);
     VPADInit();
 
-    while (1) {
+    while (ProcUIIsRunning()) {
         VPADStatus vpad;
         VPADReadError error;
         VPADRead(VPAD_CHAN_0, &vpad, 1, &error);
-
-        if (vpad.trigger & VPAD_BUTTON_HOME) break;
-
+        ProcUIProcessMessages(TRUE);
         usleep(16000);
     }
 
+    ProcUIShutdown();
     return 0;
 }
