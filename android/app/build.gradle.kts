41216a91b0cbf9390593405c20dec5aee5261a77
baldbuffalo <64604745+baldbuffalo@users.noreply.github.com>
2026-08-20 00:44:39 +0400

    Add Android app Gradle configuration

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.wavebrowser.android"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.wavebrowser.android"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.webkit:webkit:1.14.0")
+}
